e74274b11862cc8be8ee693f38fe4238efadbd36
Peter Anema <email@peteranema.nl>
2021-01-17 21:31:32 +0100

    Initial commit

---
+++ src/main/java/nl/gettoworktogether/hello_world/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package nl.gettoworktogether.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String say_hello() {
+		return "Hello world!!!";
+	}
+
+}
